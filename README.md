a2ec2fa921df71af54a612171ef0a6410136ae2a
Mat Jordan <mat@northwestern.edu>
2022-10-23 12:27:29 -0400

    Revise language.

--- README.md
+++
@@ -1,5 +0,0 @@
-# about
-
-I am a designer and developer originally from Latah County, Idaho, United States. _Latah_ is a [Nez Percé](https://www.wikidata.org/wiki/Q1123923), (Nimíipuu) word for "the place of pine trees and pestle." I currently live in Asheville, North Carolina, and work for Northwestern University Libraries, where I contribute to open-source software enhancing digital archives with a focus on user experience.
-
-Outside the office, I am likely to be found avoiding technology, tending a bonsai garden, backpacking the Blue Ridge Mountains, cooking, reading, and learning to become the best parent I can be.
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,24 +19,27 @@ const Home: NextPage = () => {
           <Header />
           <Section>
             <p>
-              <InlineHeading as="span">
-                Making things simple again.
-              </InlineHeading>{" "}
+              <InlineHeading as="span">Making things simple.</InlineHeading>{" "}
+              Creating web experiences that feel human. Imbuing natural, social,
+              and historical context deeply in my work. Toiling away.
+            </p>
+            <p>
               I am a designer and developer originally from Latah County, Idaho,
               United States. <em>Latah</em> is a{" "}
               <Link href="https://www.wikidata.org/wiki/Q1123923">
                 <a target="_blank">Nez Percé</a>
               </Link>
               , (Nimíipuu) word for &ldquo;the place of pine trees and
-              pestle.&rdquo; I currently live in Asheville, North Carolina, and
-              work for Northwestern University Libraries, where I contribute to
-              open-source software enhancing digital archives with a focus on
-              user experience.
+              pestle.&rdquo; I currently live in Asheville, North Carolina,
+              United States, and work for Northwestern University Libraries,
+              where I contribute to open-source software that enhance digital
+              archives.
             </p>
             <p>
-              Outside the office, I am likely to be found avoiding technology,
-              tending a bonsai garden, backpacking the Blue Ridge Mountains,
-              cooking, reading, and learning to become the best parent I can be.
+              When not ruminating in the office, I am likely to be found
+              avoiding technology, tending a bonsai garden, backpacking the Blue
+              Ridge Mountains, cooking, reading, and learning to become the best
+              parent I can be.
             </p>
           </Section>
         </Container>
