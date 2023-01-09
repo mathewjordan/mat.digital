2b4aa21090b5699087f9ec71dbf69ead154bf6ff
Mat Jordan <mat@northwestern.edu>
2023-01-09 15:02:43 -0500

    Add transition.

---
+++ components/Transition.tsx
@@ -0,0 +1,43 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
+
+const variants = {
+  in: {
+    opacity: 1,
+    transition: {
+      duration: 0.618,
+    },
+  },
+  out: {
+    opacity: 0,
+    transition: {
+      duration: 0.618,
+    },
+  },
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Transition: React.FC<Props> = ({ children }) => {
+  const { asPath } = useRouter();
+
+  return (
+    <div>
+      <AnimatePresence initial={true}>
+        <motion.div
+          key={asPath}
+          variants={variants}
+          animate="in"
+          initial="out"
+          exit="out"
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Transition;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import globalStyles from "../styles/global";
 import { darkTheme } from "../stitches";
+import Transition from "../components/Transition";
 import Underlay from "../components/Underlay";
 import { useEffect, useState } from "react";
 
@@ -21,8 +22,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         light: "light",
       }}
     >
-      {mounted && <Component {...pageProps} />}
       <Underlay />
+      {mounted && (
+        <Transition>
+          <Component {...pageProps} />
+        </Transition>
+      )}
     </ThemeProvider>
   );
 }
