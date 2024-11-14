e0e8619b7b39c7f79599520284a93bc09a3d36ec
Mat Jordan <mat@northwestern.edu>
2024-11-14 16:08:13 -0500

    Update workflow.

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,9 +1,16 @@
 import { Heading, Text } from "@radix-ui/themes";
 
 import { getMarkdown } from "@/lib/markdown-helpers";
+import { getPosts } from "@/lib/post-helpers";
+
+export async function generateStaticParams() {
+  const posts = await getPosts();
+  return posts.map((post) => {
+    return { slug: post.slug };
+  });
+}
 
 export default async function Post({ params }: { params: { slug: string } }) {
-  // md or mdx
   const path = "content/posts/" + params.slug + ".mdx";
   const markdown = await getMarkdown(path);
 
