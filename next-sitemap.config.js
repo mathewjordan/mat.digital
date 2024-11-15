c98d1460fe3987d97c7d1e03ac12decec2174cc9
Mat Jordan <mat@northwestern.edu>
2024-11-15 11:40:18 -0500

    Refresh sitemap.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://mat.digital",
   generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  generateIndexSitemap: false,
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
 };
